9ff9f517827c0541f65a67d1f6b90545fc2dca9f
Marcel Zelent <79370135+MarcelZelent@users.noreply.github.com>
2024-01-08 10:41:21 +0100

    Add additional plots with different K values - Ex1

--- Week2/2.1/exercise1.py
+++ Week2/2.1/exercise1.py
@@ -8,17 +8,22 @@ simlen = 30
 y_nodelay = np.zeros((simlen))
 y_delayed = np.zeros((simlen))
 y_corrected = np.zeros((simlen))
+y_minus = np.zeros((simlen))
+y_plus = np.zeros((simlen))
 # Target
 target = 0.0
 
 # Controller gain
 K = 1.0
-K_corrected = 0.1
+K_corrected = 1
+diff = 0.5
 
 # Set first output
 y_nodelay[0] = 1
 y_delayed[0] = 1
 y_corrected[0] = 1
+y_minus[0] = 1
+y_plus[0] = 1
 
 # TODO define the time delay
 delay_time = 3
@@ -36,12 +41,30 @@ for t in range(simlen-1):
     u_corrected = K_corrected * (target - y_corrected[t-delay_time])
     y_corrected[t+1]=0.5*y_corrected[t] + 0.4*u_corrected # 1st order dynamics
 
+    u_minus = (K_corrected-diff) * (target - y_minus[t-delay_time])
+    y_minus[t+1]=0.5*y_minus[t] + 0.4*u_minus # 1st order dynamics
+
+    u_plus = (K_corrected+diff) * (target - y_plus[t-delay_time])
+    y_plus[t+1]=0.5*y_plus[t] + 0.4*u_plus # 1st order dynamics
+
+
 ## Plot
 time = range(simlen)
 plt.plot(time, y_nodelay)
 plt.plot(time, y_delayed)
 plt.plot(time, y_corrected)
+plt.title(f"Time delay: {delay_time}, K_corrected: {K_corrected}")
 plt.legend(['no delay', 'delay', 'corrected'])
+plt.xlabel('time step')
+plt.ylabel('y')
+plt.show()
+
+plt.plot(time, y_nodelay)
+plt.plot(time, y_corrected)
+plt.plot(time, y_minus)
+plt.plot(time, y_plus)
+plt.title(f"Time delay: {delay_time}, K_corrected: {K_corrected}")
+plt.legend(['No delay','corrected', 'minus', 'plus'])
 plt.xlabel('time step')
 plt.ylabel('y')
 plt.show()
