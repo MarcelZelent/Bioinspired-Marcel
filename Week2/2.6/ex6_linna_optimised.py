a0215c0bf2f26cecc55cd753b7544c5896757a6f
Marcel Zelent <79370135+MarcelZelent@users.noreply.github.com>
2024-01-10 18:33:04 +0100

    Clean up ex6

--- Week2/2.6/ex6_linna_optimised.py
+++ Week2/2.6/ex6_linna_optimised.py
@@ -19,12 +19,9 @@ theta_vec = np.zeros(n_steps*n_trials)
 theta_ref_vec = np.zeros(n_steps*n_trials)
 
 ## Feedback controller variables
-# Kp = 150
-# Kv = 3
 Kp = 10
 Kv = 0
-# Kp=50
-# Kv=1
+
 
 ## TODO: Define parameters for periodic reference trajectory
 A = np.pi
@@ -70,10 +67,9 @@ for i in range(n_steps*n_trials):
     
     x = [theta_ref,theta]
     tau_cmac = c. predict(x)
-    # print("cmac",tau_cmac)
+ 
     tau = tau_m + tau_cmac
-    # print("taum",tau_m)
-    # print("tau",tau)
+
     # Iterate simulation dynamics
     c.learn(tau_m)
     plant.step(tau)
@@ -83,50 +79,15 @@ for i in range(n_steps*n_trials):
     mean_before = mean_now
     mean_vec.append(mean_now)
     
-    # print("e_vec",mse)
-    
   
     epoch = epoch +1
-    # # print(epoch)
-    # if (flag == 0 and epoch > 100):
-    #     if mse<= 0.1:
-    #         print("epoch",epoch)
-    #         flag = 1
-    #         i_low = i
-    #         # T = 2
-    
-    # if flag == 1:
-    #     if (i>i_low+n_steps):
-    #         print(T)
-    #         print(mse)
-    #         break
 
     if i%rate == 0:
-        print(i/rate, mean_now)
-            
-                
-            
-        
+        print(i/rate, mean_now)    
     
     theta_vec[i] = plant.theta
     theta_ref_vec[i] = theta_ref
 
-    # for _ in range(1000):
-    #     e_vec = []
-    #     for x1 in np.linspace(0, 1, 11):
-    #         for x2 in np.linspace(0, 1, 11):
-    #             x = [x1, x2]
-
-    #             yhat = c.predict(x)
-
-    #             yd = np.arctan2(x[0], x[1])
-
-    #             e = yd - yhat
-
-    #             c.learn(e)
-    #             e_vec.append(e**2)
-
-        # print(np.mean(e_vec))
 
 ## Plotting
 
@@ -136,14 +97,5 @@ plt.plot(t_vec, theta_vec, label='theta')
 plt.plot(t_vec, theta_ref_vec, '--', label='reference')
 plt.plot(t_vec, mean_vec, label='error')
 plt.legend()
-
-## Plot trial error
-#error_vec = theta_ref_vec - theta_vec
-#l = int(T/Ts)
-#trial_error = np.zeros(n_trials)
-#for t in range(n_trials):
-#    trial_error[t] = np.sqrt( np.mean( error_vec[t*l:(t+1)*l]**2 ) )
-#plt.figure()
-#plt.plot(trial_error)
 
 plt.show()
